e4137d8842f2b40e8621c8854af10cbe23946860
theuszastro <matheuszastrodev@hotmail.com>
2022-02-17 02:09:04 -0300

    changed .gitignore

--- Compiler/index.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'fs/promises';
-import { compiler } from './src/compiler.mjs';
-
-(async () => {
-	const content = await fs.readFile('./data.moon', 'utf-8');
-
-	compiler('data.moon', content);
-})();
